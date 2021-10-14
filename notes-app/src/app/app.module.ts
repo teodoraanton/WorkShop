dac949344b7d888476d570f8ad73b716fa3812e7
teodoraanton <antonteodora0@gmail.com>
2021-10-14 09:37:32 +0300

    Workshop - Day 4

--- notes-app/src/app/app.module.ts
+++ notes-app/src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Curs3Component } from './curs3/curs3.component';
 import { TestModule } from './test/test.module';
+import { NoteComponent } from './note/note.component';
+import { ToolsComponent } from './tools/tools.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Curs3Component
+    Curs3Component,
+    NoteComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    TestModule
+    TestModule,
+    NoteComponent,
+    ToolsComponent,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ notes-app/src/app/tools/tools.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tools',
+  templateUrl: './tools.component.html',
+  styleUrls: ['./tools.component.scss']
+})
+export class ToolsComponent implements OnInit {
+
+  title: string = "Something";
+  textColor: string = "red";
+  noteContent: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ButtonClick(): void{
+    /*alert("Button Click");*/
+    this.title = "Something else";
+    this.textColor = "blue";
+  }
+}
